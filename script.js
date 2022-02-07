79048b174ea54ac21b9000c5c0999840a1df98f8
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-07 15:59:16 +0600

    changing  styles with js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('This is all me');
+const blogTitle = document.getElementsByTagName('h2');
+const blogDetails = document.getElementsByTagName('p');
+const bTitle = document.getElementById('btitle');
+console.log(blogTitle);
+
+for (const h2 of blogTitle) {
+    h2.style.color = 'green';
+    h2.style.textAlign = 'center';
+}
+
+for (const p of blogDetails) {
+    p.style.background = 'yellow';
+    p.style.textAlign = 'center';
+
+}
+bTitle.style.color = 'darkblue';
+bTitle.style.fontStyle = 'italic';
